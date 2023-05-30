664dc95c5b6bbe2c361554c15493a08b8eff246c
ychong032 <yowlimchong2403@gmail.com>
2023-05-30 22:11:26 +0800

    Add ability to expand tasks to view details

--- src/index.js
+++ src/index.js
@@ -20,14 +20,14 @@ newTaskForm.addEventListener("submit", addNewTask);
 setMinDate();
 
 function showNewTaskForm() {
-	newTaskForm.style.visibility = "visible";
-	newTaskButton.style.visibility = "hidden";
+	newTaskForm.style.display = "flex";
+	newTaskButton.style.display = "none";
 }
 
 function hideNewTaskForm() {
 	newTaskForm.reset();
-	newTaskForm.style.visibility = "hidden";
-	newTaskButton.style.visibility = "visible";
+	newTaskForm.style.display = "none";
+	newTaskButton.style.display = "block";
 }
 
 function addNewTask() {
@@ -46,13 +46,83 @@ function addNewTask() {
 	allTodos.push(newTask);
 	console.log(allTodos);
 
-	const newTaskElement = document.createElement("div");
-	newTaskElement.classList.add("task");
-	newTaskElement.textContent = newTask.title;
+	const newTaskElement = createNewTaskElement(
+		newTask.title,
+		newTask.description,
+		newTask.priority
+	);
 
 	taskGrid.appendChild(newTaskElement);
 
 	hideNewTaskForm();
+}
+
+function createNewTaskElement(title, description, priority) {
+	const newTaskElement = document.createElement("div");
+	newTaskElement.classList.add("task");
+
+	const newTaskHeader = document.createElement("div");
+	newTaskHeader.classList.add("task-header");
+	newTaskHeader.addEventListener("click", toggleDetails);
+
+	const newTaskTitle = document.createElement("div");
+	newTaskTitle.classList.add("task-title");
+	newTaskTitle.textContent = title;
+
+	const newTaskExpand = document.createElement("div");
+	newTaskExpand.classList.add("expand-task");
+	newTaskExpand.textContent = "+";
+
+	const newTaskDetails = document.createElement("div");
+	newTaskDetails.classList.add("task-details");
+	newTaskDetails.style.display = "none";
+
+	const newTaskDescription = document.createElement("div");
+	newTaskDescription.classList.add("description");
+
+	const newTaskDescriptionHeader = document.createElement("h4");
+	newTaskDescriptionHeader.textContent = "Description";
+
+	const newTaskDescriptionText = document.createElement("p");
+	newTaskDescriptionText.textContent =
+		description === "" ? "(no description provided)" : description;
+
+	const newTaskPriority = document.createElement("div");
+	newTaskPriority.classList.add("priority");
+
+	const newTaskPriorityHeader = document.createElement("h4");
+	newTaskPriorityHeader.textContent = "Priority";
+
+	const newTaskPriorityText = document.createElement("p");
+	newTaskPriorityText.textContent = priority;
+
+	newTaskPriority.appendChild(newTaskPriorityHeader);
+	newTaskPriority.appendChild(newTaskPriorityText);
+
+	newTaskDescription.appendChild(newTaskDescriptionHeader);
+	newTaskDescription.appendChild(newTaskDescriptionText);
+
+	newTaskDetails.appendChild(newTaskDescription);
+	newTaskDetails.appendChild(newTaskPriority);
+
+	newTaskHeader.appendChild(newTaskTitle);
+	newTaskHeader.appendChild(newTaskExpand);
+
+	newTaskElement.appendChild(newTaskHeader);
+	newTaskElement.appendChild(newTaskDetails);
+
+	return newTaskElement;
+}
+
+function toggleDetails(e) {
+	console.log(e);
+
+	const taskDetails = e.target.nextSibling;
+	taskDetails.style.display =
+		taskDetails.style.display === "none" ? "flex" : "none";
+
+	const expandTask = e.target.lastChild;
+	expandTask.textContent = expandTask.textContent === "+" ? "-" : "+";
 }
 
 function setMinDate() {
@@ -60,6 +130,3 @@ function setMinDate() {
 	const dueDateElement = document.querySelector("#dueDate");
 	dueDateElement.min = minDate;
 }
-
-// Next step: handle form validation and submission
-// TODO: handle min allowed date for due date
