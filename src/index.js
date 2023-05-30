54b5161c45dcd853c3567e0ec284c584933cd031
ychong032 <yowlimchong2403@gmail.com>
2023-05-30 19:40:46 +0800

    Add ability to add new tasks to All Tasks page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "./style.css";
 import { format, parse } from "date-fns";
+import { Todo } from "./todo";
 
 // Arrays to store each list of Todo objects
 const allTodos = [];
@@ -13,15 +14,51 @@ newTaskButton.addEventListener("click", showNewTaskForm);
 const cancelNewTaskButton = document.querySelector("#newTaskButtons > .cancel");
 cancelNewTaskButton.addEventListener("click", hideNewTaskForm);
 
+const newTaskForm = document.querySelector("#newTaskForm");
+newTaskForm.addEventListener("submit", addNewTask);
+
+setMinDate();
+
 function showNewTaskForm() {
-	const newTaskForm = document.querySelector("#newTaskForm");
-	console.log(newTaskForm);
 	newTaskForm.style.visibility = "visible";
+	newTaskButton.style.visibility = "hidden";
 }
 
 function hideNewTaskForm() {
-	const newTaskForm = document.querySelector("#newTaskForm");
+	newTaskForm.reset();
 	newTaskForm.style.visibility = "hidden";
+	newTaskButton.style.visibility = "visible";
+}
+
+function addNewTask() {
+	let taskGrid = document.querySelector("#task-grid");
+	let titleElement = document.querySelector("#title");
+	let descriptionElement = document.querySelector("#description");
+	let dueDateElement = document.querySelector("#dueDate");
+	let priorityElement = document.querySelector("#priority");
+
+	let newTask = new Todo(
+		titleElement.value,
+		descriptionElement.value,
+		dueDateElement.value,
+		priorityElement.value
+	);
+	allTodos.push(newTask);
+	console.log(allTodos);
+
+	const newTaskElement = document.createElement("div");
+	newTaskElement.classList.add("task");
+	newTaskElement.textContent = newTask.title;
+
+	taskGrid.appendChild(newTaskElement);
+
+	hideNewTaskForm();
+}
+
+function setMinDate() {
+	const minDate = format(new Date(), "yyyy-MM-dd");
+	const dueDateElement = document.querySelector("#dueDate");
+	dueDateElement.min = minDate;
 }
 
 // Next step: handle form validation and submission
